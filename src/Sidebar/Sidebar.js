5e3e4cc768fa435608b1c6df02287cf32a402037
louisnguyen2005 <kinguyen291118@gmail.com>
2024-09-23 11:59:51 +0700

    second

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import Category from "./Categogy/Categogy";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+import "./Sidebar.css";
+
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <h1>🛒</h1>
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
